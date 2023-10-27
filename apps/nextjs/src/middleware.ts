7d10d1f8f8a2f2c7224074986c91392bde5229c9
oggnimodd <arnold.ds.dev@gmail.com>
2023-10-27 16:58:46 +0700

    add redirection logic

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -1,10 +1,36 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/sign-up"],
+  publicRoutes: ["/", "/sign-in", "/sign-up", "/s/"],
   debug: false,
+  afterAuth: async (auth, req: NextRequest, event: NextFetchEvent) => {
+    const isSlugUrl = req.nextUrl.pathname.indexOf("/s/") === 0;
+
+    if (isSlugUrl) {
+      // Get pathname:
+      const slug = req.nextUrl.pathname.split("/").pop();
+
+      // Get data from query:
+      const data = await fetch(`${req.nextUrl.origin}/api/url/${slug}`);
+
+      // Return (/) if not found (404):
+      if (data.status === 404) {
+        return NextResponse.redirect(req.nextUrl.origin);
+      }
+
+      // Convert data to JSON:
+      const dataToJson = await data.json();
+
+      if (data?.url) {
+        return NextResponse.redirect(new URL(dataToJson.url));
+      }
+    }
+
+    return NextResponse.next();
+  },
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)", "/s/:slug*"],
 };
