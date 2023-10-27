9b0bc4564d4072f294719f05a68a5b12b32a28ce
oggnimodd <arnold.ds.dev@gmail.com>
2023-10-27 13:53:33 +0700

    log posts

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
 import { Layout } from "components";
+import { api } from "utils";
 
 const index: NextPage = () => {
+  const posts = api.post.all.useQuery();
+
+  console.log(posts);
+
   return (
     <Layout>
       <p>Hello, World!</p>
