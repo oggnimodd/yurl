057cb0daee806399ae53d05e34921f7b2bca1de9
oggnimodd <arnold.ds.dev@gmail.com>
2023-11-02 13:04:13 +0700

    vue-app: setup storybook

---
+++ apps/vue/src/ui/Button/Button.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Button from 'primevue/button';
+
+export interface Props {
+  label: string
+  disabled?: boolean
+}
+
+const { label } = withDefaults(defineProps<Props>(), {
+  disabled: false
+})
+
+</script>
+
+<template>
+  <Button :disabled="disabled">{{ label }}</Button>
+</template>
