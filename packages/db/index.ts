5294169a01054e8d8cdd99db9c719bcfbc19c76b
oggnimodd <arnold.ds.dev@gmail.com>
2023-11-10 16:47:31 +0700

    fix build error

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 // 1. Import libSQL and the Prisma libSQL driver adapter
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client";
